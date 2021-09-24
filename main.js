fc8bd5014fc617ffecd4dc7c5b8c316522206778
gamerrdude <87709109+gamerrdude@users.noreply.github.com>
2021-09-24 19:20:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+webcam.set({
+    width:300,
+    height:300,
+    image_format: 'png',
+    png_quality:90
+})
+
+camera = document.getElementById("camera");
+webcam.attach( '#camera' );
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id = "captured_image" src = "'+data_uri+'"/>';
+    });
+}
+console.log('ml5 version', ml5.version);
